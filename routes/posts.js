3bd070c53a359c97fce194f7209bb487e1c2000c
Nick <linshuan880727@gmail.com>
2024-09-17 17:11:32 +0800

    update

---
+++ routes/posts.js
@@ -0,0 +1,65 @@
+// routes/posts.js
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const Post = require('../models/post');
+const { Storage } = require('@google-cloud/storage');
+
+// 設定圖片上傳存儲
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// 創建新帖子
+router.post('/', upload.single('image'), async (req, res) => {
+  const { content } = req.body;
+  if (!content) {
+    return res.status(400).send('Content is required');
+  }
+
+  try {
+    const imageUrl = req.file ? await uploadImageToGCS(req.file) : null;
+    const newPost = new Post({
+      content,
+      image: imageUrl
+    });
+    await newPost.save();
+    res.status(201).json({ message: 'Post created successfully', data: newPost });
+  } catch (error) {
+    console.error('Error creating post:', error);
+    res.status(500).send('Error creating post: ' + error.message);
+  }
+});
+
+async function uploadImageToGCS(file) {
+  const bucketName = 'nick_product_bucket';
+  const storage = new Storage({
+    keyFilename: process.env.GOOGLE_CLOUD_KEYFILE //modified
+  });
+  const bucket = storage.bucket(bucketName);
+  const blob = bucket.file(Date.now() + path.extname(file.originalname));
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+  });
+
+  return new Promise((resolve, reject) => {
+    blobStream.on('error', reject);
+    blobStream.on('finish', () => {
+      const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+      resolve(publicUrl);
+    });
+    blobStream.end(file.buffer);
+  });
+}
+
+// 獲取所有帖子
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.send(posts);
+  } catch (error) {
+    res.status(500).send('Error fetching posts: ' + error.message);
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,43 @@
+  const express = require('express');
+  const mongoose = require('mongoose');
+  const userRoutes = require('./routes/users');
+  const postRoutes = require('./routes/posts');
+  const uploadRoutes = require('./routes/upload');
+  const cors = require('cors');
+  const path = require('path');
+  require('dotenv').config();
+
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  // 连接到 MongoDB
+  // mongodb+srv://linshuan880727:<db_password>@nickserver.0wgra.mongodb.net/?retryWrites=true&w=majority&appName=NickServer
+  const dbURI = process.env.MONGODB_URI;
+  mongoose.connect(dbURI);
+
+  // 检查连接
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+  });
+  mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+  app.use(cors({
+      origin: "https://nickproduct.netlify.app" || 'http://localhost:5173', // 允许此来源的请求
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // 允许的 HTTP 方法
+      allowedHeaders: ['Content-Type', 'Authorization'] // 允许的标头
+  }));
+
+  // 中间件
+  app.use(express.json());
+
+  // 设置路由
+  app.use('/upload', uploadRoutes);
+  app.use('/users', userRoutes);
+  app.use('/posts', postRoutes);
+
+  // 启动服务器
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
